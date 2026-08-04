b3ce35600e19ff6657394b255f10daebc36d0d3c
Vinit080 <119064897+Vinit080@users.noreply.github.com>
2026-08-04 23:31:11 +0530

    chore: remove unnecessary IDE and scratch files

--- check.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('./src/generated/prisma/client');
-const prisma = new PrismaClient();
-
-async function check() {
-  const users = await prisma.user.findMany();
-  console.log("Users:", users.map(u => ({ email: u.email, role: u.role, hash: u.passwordHash })));
-}
-
-check().catch(console.error).finally(() => prisma.$disconnect());
